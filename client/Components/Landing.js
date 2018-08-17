e814c89b00839871cdde37d2547950d51739c737
Roh Won Seok <wonseok96@gmail.com>
2018-08-17 15:34:15 +0900

    landing.js

--- client/Components/Landing.js
+++ client/Components/Landing.js
@@ -1,31 +1,43 @@
 import React, { Component } from "react";
-import { View, Text, Image, StyleSheet, Alert } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Alert,
+  AsyncStorage,
+  Button
+} from "react-native";
+import SocketIOClient from "socket.io-client";
+import Store from "./store";
 
-export default class Loading extends Component {
-  state = {
-    latitude: 0,
-    longitude: 0
-  };
+export default class Landing extends Component {
+  constructor(props) {
+    super(props);
+    //this.socket = SocketIOClient("http://localhost:3000");
+  }
+
   static navigationOptions = {
     header: null
   };
   componentDidMount() {
     // Start counting when the page is loaded
-    navigator.geolocation.getCurrentPosition(position => {
-      var lat = parseFloat(position.coords.latitude);
-      var long = parseFloat(position.coords.longitude);
-      this.setState({
-        latitude: lat,
-        longitude: long
-      });
-    });
+    //this._what();
+    // navigator.geolocation.getCurrentPosition(position => {
+    //   var lat = parseFloat(position.coords.latitude);
+    //   var long = parseFloat(position.coords.longitude);
+    //   this.setState({
+    //     latitude: lat,
+    //     longitude: long
+    //   });
+    // });
     this.timeoutHandle = setTimeout(() => {
       // Alert.alert(
       //   "위도 : " + JSON.stringify(this.state.latitude),
       //   "경도 : " + JSON.stringify(this.state.longitude)
       // );
-      this.props.navigation.navigate("SelectCatScreen");
-    }, 50);
+      this._userInfo();
+    }, 5000);
   }
 
   componentWillUnmount() {
@@ -35,6 +47,17 @@ export default class Loading extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Store.Consumer>
+          {store => (
+            <Button
+              onPress={() => {
+                this._monkey(store);
+              }}
+              title="see store"
+              color="#841584"
+            />
+          )}
+        </Store.Consumer>
         <Text style={styles.title}>슈뢰딩거의 고양이</Text>
         <Image
           style={{ width: 300, height: 300 }}
@@ -46,22 +69,30 @@ export default class Loading extends Component {
     );
   }
 
-  _userInfo = () => {
-    //db로 위도, 경도, 토큰 보내주기
-    if (this.state.latitude === "" || this.state.longitude === "") {
-      return;
+  //   _what = async () => {
+  //     const token = await AsyncStorage.getItem("token");
+  //     console.log(token);
+  //   };
+  _monkey = something => {
+    something.emit("msg", "fucking");
+  };
+  _userInfo = async () => {
+    //위도, 경도, 토큰 보내주기
+    await AsyncStorage.setItem("token", "");
+    try {
+      const token = await AsyncStorage.getItem("token");
+      if (!token) {
+        this.props.navigation.navigate("SelectCatScreen");
+      } else {
+        // this.socket.emit("", token);
+        // this.socket.on("", userinfo=>{
+        //     await AsyncStorage.setItem("userInfo", JSON.stringify(userinfo));
+        // })
+        this.props.navigation.navigate("OpenBoxScreen");
+      }
+    } catch (err) {
+      console.log(err);
     }
-    fetch("link", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        latitude: this.state.latitude,
-        longitude: this.state.longitude
-      })
-    });
   };
 }
 
