bf47e22fba3e1b7791085287226d734ba72bd706
cosmos2 <playnstop.s@gmail.com>
2018-08-20 19:12:56 +0900

    timer design changed

--- client/Components/Timer/presenter.js
+++ client/Components/Timer/presenter.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
+import { Icon } from "react-native-elements";
 
 export default class TimePicker extends Component {
   componentWillReceiveProps() {
@@ -11,6 +12,7 @@ export default class TimePicker extends Component {
   render() {
     return (
       <View style={styles.timer}>
+        <Icon name="md-time" type="ionicon" color="black" size={21} />
         <Text style={styles.text}>{this.props.time}</Text>;
       </View>
     );
